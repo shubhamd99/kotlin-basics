bc385e003e7622d2626a716749617112498da4e4
Shubham Dhage <shubham.dhage@swiggy.in>
2021-02-24 04:35:44 +0530

    data types

---
+++ src/DataTypes.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    // Read only
+    val str: String = "Shubham"
+
+    val str1: String
+    str1 = "Dhage"
+
+    println(str)
+    println("Name: $str $str1")
+
+    // Read and write
+    var count = 1
+    println("count $count")
+    count = 10
+    println("count $count")
+
+    // Optional
+    var department: String?
+    department = null
+    department = "Software Engineering"
+    println("department ${department!!}") // non-null assertion (!!)
+}
