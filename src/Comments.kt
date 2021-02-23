f2468c476e2f8735b48d251f6ca67b7d21272646
Shubham Dhage <shubham.dhage@swiggy.in>
2021-02-24 04:43:55 +0530

    comments

---
+++ src/Comments.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // This is a single line comment
+    println("Hello World!")
+
+    /* This is a multi-line comment. I'm writing this
+    * text to show you that we can write the comments
+    * in multiple lines
+    */
+    println("Hello World!")
+}
